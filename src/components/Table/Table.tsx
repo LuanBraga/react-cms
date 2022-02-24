6a81e05113f000efff8fb0b9573c5d0bd6ff0719
LuanBraga <luanbraga.lb@gmail.com>
2022-02-24 15:18:07 -0300

    implement table

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,57 +1,66 @@
-import {useMemo} from "react"
-import { useTable, Column } from "react-table"
+// import Icon from "@mdi/react"
+// import { mdiOpenInNew } from '@mdi/js';
 
-export interface DataInterface{
-    col1: string,
-    col2: string,
-}
+// import {useMemo} from "react"
+import { useTable, Column, TableInstance } from "react-table"
 
-export default function Table(){
-    const data = useMemo<DataInterface[]>(
-        ()=> [
-            {
-                col1: 'Hello',
-                col2: 'World'
-            },
-            {
-                col1: 'React-table',
-                col2: 'rocks'
-            },
-            {
-                col1: 'whatever',
-                col2: 'you want',
-            },
-        ],
-        []
-    )
+import * as T from "./Table.styles"
 
-    const columns = useMemo<Column<DataInterface>[]>(
-        ()=> [
-            {
-                Header: 'col1'
-            },
-            {
-                Header: 'Column 2',
-                acessor: 'col2'
-            },
-        ],
-        []
-    )
 
-    const tableInstance = useTable<DataInterface>({data, columns})
+export default function Table<T extends Object>({instance}: {instance: TableInstance<T>}){
+    
+
+    //UseTable recebe data e columns "memoizados"
+    //UseTable retorna um obj table instance
+    //Esse objeto contém tudo que é necessário para contruir uma tabela.
+    // const tableInstance = useTable<DataType>({data, columns})
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        prepareRow,
+        headerGroups,
+        rows,
+    } = instance
     
     return(
-        <table>
-            <thead>
-                <tr>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td></td>
-                </tr>
-            </tbody>
-        </table>
+        //aplicando as propriedades da tabela
+        <T.TableWrapper cellPadding='0' cellSpacing='0' {...getTableProps()}>
+            <T.Heading>
+                { //percorrendo através das linhas de cabeçalho
+                    headerGroups.map(headerGroup => (
+                        //aplicando as propriedades das linhas de cabeçalho
+                        <T.HeadingRow {...headerGroup.getHeaderGroupProps()}>
+                            {//percorrendo através dos cabeçalhos em cada linha
+                                headerGroup.headers.map(column => (
+                                    //applicando as propriedades da célula de cabeçalho
+                                    <T.HeadingCell {...column.getHeaderProps()}>
+                                        {//renderizando o cabeçalho
+                                            column.render('Header')
+                                        }
+                                    </T.HeadingCell>
+                                ))   
+                            }
+                        </T.HeadingRow>
+                    ))
+                }
+            </T.Heading>
+            <T.Body {...getTableBodyProps()}>
+                {
+                    rows.map(row => {
+                        prepareRow(row)
+                        return <T.BodyRow {...row.getRowProps()}>
+                            {
+                                row.cells.map(cell => {
+                                    return <T.BodyCell {...cell.getCellProps()}>
+                                        {cell.render('Cell')}
+                                    </T.BodyCell>
+                                })
+                            }
+                        </T.BodyRow>
+                    })
+                }
+            </T.Body>
+        </T.TableWrapper>
     )
 }
---
+++ src/stories/Table.stories.tsx
@@ -0,0 +1,125 @@
+import Icon from "@mdi/react";
+import { mdiOpenInNew } from "@mdi/js"
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { useMemo } from "react";
+import Table from "../components/Table/Table";
+import { Column, useTable } from "react-table";
+
+export default {
+    title: 'Components/Table',
+    component: Table,
+} as ComponentMeta<typeof Table>;
+
+// const Template: ComponentStory<typeof Table> = (args) => <Table {...args} />;
+
+// export const Default = Template.bind({});
+// Default.args = {
+// };
+
+export type DataType = {
+    preview: React.ReactNode
+    col1: string
+    col2: string
+    actions: string
+}
+
+export function Default () {
+    //É importante usar o useMemo() pra garantir que nosso "data" não seja recriado e recalculado a cada renderização
+    const data = useMemo<DataType[]>(
+        () => [
+            {
+              col1: 'Hello',
+              col2: 'World',
+              actions: 'action',
+              preview: <Icon 
+                        size="14px"
+                        color="#09F"
+                        path={mdiOpenInNew}/>
+            },
+            {
+              col1: 'react-table',
+              col2: 'rocks',
+              actions: 'action',
+              preview: <Icon 
+                        size="14px"
+                        color="#09F"
+                        path={mdiOpenInNew}/>
+            },
+            {
+              col1: 'whatever',
+              col2: 'you want',
+              actions: 'action',
+              preview: <Icon 
+                        size="14px"
+                        color="#09F"
+                        path={mdiOpenInNew}/>
+            },
+          ],
+          []
+        )
+
+    //Setando as definições de coluna a serem passadas para useTable()
+    const columns = useMemo<Column<DataType>[]>(
+        () => [
+            {
+                Header: '',
+                accessor: 'preview',
+            },
+            {
+                Header: 'Column 1',
+                accessor: 'col1', //acessor é a chave do objeto em "data"
+                Cell: (row) => <div style={{textAlign: 'right'}}>{row.value}</div>
+            },
+            {
+                Header: 'Column 2',
+                accessor: 'col2',
+                Cell: (row) => <div style={{textAlign: 'center'}}>{row.value}</div>
+            },
+            {
+                Header: 'Actions',
+                accessor: 'actions',
+            },
+        ],
+        []
+    )
+
+    const tableInstance = useTable<DataType>({data, columns})
+    
+    return <Table<DataType> instance={tableInstance}/>
+}
+
+export function NoData() {
+    const data = useMemo<DataType[]>(
+        () => [],
+          []
+        )
+
+    //Setando as definições de coluna a serem passadas para useTable()
+    const columns = useMemo<Column<DataType>[]>(
+        () => [
+            {
+                Header: '',
+                accessor: 'preview',
+            },
+            {
+                Header: 'Column 1',
+                accessor: 'col1', //acessor é a chave do objeto em "data"
+                Cell: (row) => <div style={{textAlign: 'right'}}>{row.value}</div>
+            },
+            {
+                Header: 'Column 2',
+                accessor: 'col2',
+                Cell: (row) => <div style={{textAlign: 'center'}}>{row.value}</div>
+            },
+            {
+                Header: 'Actions',
+                accessor: 'actions',
+            },
+        ],
+        []
+    )
+
+    const tableInstance = useTable<DataType>({data, columns})
+    
+    return <Table<DataType> instance={tableInstance}/>
+}
